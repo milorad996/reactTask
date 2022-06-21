13a2a09fa4d94056450160bfa6d913b6d720ee5d
milorad996 <mips96.1@gmail.com>
2022-06-21 16:19:17 +0200

    Zadatak 1

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
+
+
+  const animals = [
+    { vrsta: 'Pas', name: 'Boni', datumRodjenja: new Date() },
+    { vrsta: 'Majmun', name: 'Moni', datumRodjenja: new Date() },
+    { vrsta: 'Pingvin', name: 'Riki', datumRodjenja: new Date() },
+    { vrsta: 'Zirafa', name: 'Spongi', datumRodjenja: new Date() },
+    { vrsta: 'Konj', name: 'Poni', datumRodjenja: new Date() },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Zivotinje</h1>
+
+      {animals.map((animal, index) => (
+        <table>
+
+          <tr key={index}>
+            <td>{animal.vrsta}</td>
+            <td>{animal.name}</td>
+            <td>{animal.datumRodjenja.toDateString()}</td>
+          </tr>
+
+        </table>
+
+      ))}
+
     </div>
   );
 }
